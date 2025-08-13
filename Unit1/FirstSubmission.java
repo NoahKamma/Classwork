19f6d5a5611544841b6bde995da2c4545ed3914a
NoahKamma <salesnoah1@gmail.com>
2025-08-13 08:33:46 -1000

    Create FirstSubmission.java

---
+++ Unit1/FirstSubmission.java
@@ -0,0 +1,23 @@
+//TASK 1: Print out "Hello World"
+
+// FileName: "HelloWorld.java"
+public class Main {
+    
+    //This prints Hello World
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+    }
+}
+
+//TASK 2: Print out "Hello World" and then print it again on the next line
+
+public class Main {
+    
+    //This prints Hello World twice on two different lines
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+    }
+}
