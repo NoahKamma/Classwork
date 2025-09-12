b156eec8e5b950592bf8c123a377ba984ea08cb1
NoahKamma <salesnoah1@gmail.com>
2025-09-12 10:12:54 -1000

    Update CodingBat1.java

--- Unit1/CodingBat1.java
+++ Unit1/CodingBat1.java
@@ -1,23 +1,36 @@
 //helloName
 
+/*
+*This returns your name within the string "Hello (your name)!"
+*/
 public String helloName(String name) {
   return "Hello " + name + "!";
 }
 
 //makeAbba
 
+/*
+*This returns a string within the order "a+b+b+a"
+*/
 public String makeAbba(String a, String b) {
   return a+b+b+a;
 }
 
 //makeTags
 
+
+/*
+*
+*/
 public String makeTags(String tag, String word) {
   return "<"+tag+">"+word+"<"+"/"+tag+">";
 }
 
 //firstHalf
 
+/*
+*
+*/
 public String firstHalf(String str) {
   int perfectHalf = (str.length())/2;
   return str.substring(0,perfectHalf);
