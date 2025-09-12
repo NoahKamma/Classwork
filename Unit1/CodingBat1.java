35195792e8d04d453a3eb596b7ab868992622f7b
NoahKamma <salesnoah1@gmail.com>
2025-09-12 08:09:16 -1000

    Create CodingBat1.java

---
+++ Unit1/CodingBat1.java
@@ -0,0 +1,24 @@
+//helloName
+
+public String helloName(String name) {
+  return "Hello " + name + "!";
+}
+
+//makeAbba
+
+public String makeAbba(String a, String b) {
+  return a+b+b+a;
+}
+
+//makeTags
+
+public String makeTags(String tag, String word) {
+  return "<"+tag+">"+word+"<"+"/"+tag+">";
+}
+
+//firstHalf
+
+public String firstHalf(String str) {
+  int perfectHalf = (str.length())/2;
+  return str.substring(0,perfectHalf);
+}
