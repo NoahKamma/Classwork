ef60dc484b7969075ec0b243e872a529df6cdc60
NoahKamma <salesnoah1@gmail.com>
2025-10-01 09:18:40 -1000

    Update StringCodingbat2.java

--- Unit1/StringCodingbat2.java
+++ Unit1/StringCodingbat2.java
@@ -1,5 +1,35 @@
 //makeOutWord answer
-public String makeOutWord(String out, String word) {
+public String makeOutWord(String out, String word) 
+{
   return out.substring(0,2)+word+out.substring(2,4);
 }
 
+//firstTwo answer
+public String firstTwo(String str) 
+{
+  
+  if (str.length() > 2)
+  {
+        return str.substring(0,2);    
+  }
+  else
+  {
+      return str.substring(0,str.length());
+  }
+
+  
+}
+
+//extraEnd answer
+public String extraEnd(String str) {
+  String lastTwo = new String(str.substring(str.length()-2,str.length()));
+  return lastTwo+lastTwo+lastTwo;
+}
+
+//withoutEnd
+public String withoutEnd(String str) {
+  
+  int length = str.length();
+  return str.substring(1,length-1);
+  
+}
