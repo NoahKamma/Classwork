862f0ad0b9cb18e6e09a74f43758ca4e2f70005a
NoahKamma <salesnoah1@gmail.com>
2025-10-15 12:45:38 -1000

    Create LogicCodingbat1.java

---
+++ Unit1/LogicCodingbat1.java
@@ -0,0 +1,20 @@
+/*
+* cigarParty
+*/
+public boolean cigarParty(int cigars, boolean isWeekend) {
+  if(isWeekend == true && cigars>=40)
+  {
+    return true;
+  }
+  else
+  {
+    if (cigars>=40 && cigars<=60)
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+}
