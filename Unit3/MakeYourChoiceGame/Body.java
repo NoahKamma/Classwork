13c9cd0266a1c876f2aabef351f8e65f4730099f
NoahKamma <salesnoah1@gmail.com>
2025-12-15 07:44:49 -1000

    Create Body.java

---
+++ Unit3/MakeYourChoiceGame/Body.java
@@ -0,0 +1,51 @@
+package MakeYourChoiceGame;
+
+public class Body {
+	public String waiver()
+	{
+		return "---------------------------------------------------------------------------------------------------------------------\n"
+				+ "LIABILITY WAIVER OPERATIONS & OPERATIONS DISCLOSURE AGREEMENT\n"
+				+ " \n"
+				+ "PURPOSE & SCOPE\n"
+				+ "THIS AGREEMENT (“AGREEMENT”) SERVES TO:\n"
+				+ "ESTABLISH THAT THE COMPANY AND ITS EMPLOYERS IS NOT RESPONSIBLE FOR ANY DAMAGES DEATH THAT\n"
+				+ "MAY BEFALL UPON THE EMPLOYEE AND THAT THEIR SAFETY IS NOT GUARANTEED\n"
+				+ "DOCUMENT THE ACKNOWLEDGEMENT THAT THE EMPLOYEE RESIGNS ANY AND ALL HUMANS RIGHTS.\n"
+				+ "DOCUMENT THE RESPONSIBILITY THAT THE EMPLOYEE HOLDS REGARDING\\n "
+				+ "CONFIDENTIALITY ON COMPANY POLICIES, ACTIVITY AND EXISTENCE\n"
+				+ " \n"
+				+ "DEFINITIONS\n"
+				+ "For the purposes of this Agreement:\n"
+				+ "“The Company” refers to the company, its employers, its employees and its clients\n"
+				+ "“Activity” refers to any task, assignment, directive, or mysterious late-night briefing provided by The Company.\n"
+				+ "“Asset Reallocation Measures” refers strictly to inventory management,\n "
+				+ "and not to anything resembling unauthorized redistribution of property.\n"
+				+ "“Field Engagements” include all off-site tasks, including those involving unmarked vehicles,\n "
+				+ "encrypted messages, or quiet nods exchanged in alleyways.\n"
+				+ "“Bystanders” refers to any individual or organization that are not associated with The Company\n"
+				+ " \n"
+				+ "EMPLOYEE ACKNOWLEDGEMENT OF OPERATIONAL AMBIGUITY\n"
+				+ "I hereby acknowledge that:\n"
+				+ "Any ethical and moral implications do not apply to whatever activities or\n"
+				+ "their backgrounds that are provided and will not talk or associate with them for the remainder of my employment.\n"
+				+ "I, the individual, take full responsibility for whatever harm and death that\n "
+				+ "befalls upon bystanders or myself, in all matters of physical or mental.\n"
+				+ "I shall not question nor interfere with whatever activity I am assigned.\n"
+				+ " \n"
+				+ "CONFEDENTIALITY AGREEMENT\n"
+				+ "I agree to maintain absolute confidentiality regarding:\n"
+				+ "The existence of the Company or any activities associated with it\n"
+				+ "The Company’s methods, members, and snack preferences.\n"
+				+ "The existence of fellow employees, employers, clients or\n "
+				+ "individuals that support or of interest to the Company\n"
+				+ "Failure to maintain confidentiality will result in disciplinary action, up to and including:\n"
+				+ "The withdrawal of all potential and current funds allocated by the employee from the Company.\n"
+				+ "The immediate termination of employment and human life.\n"
+				+ " \n"
+				+ "Applicant sign name in field:\n"
+				+"---------------------------------------------------------------------------------------------------------------------\n";
+	}
+	
+	
+	
+}
