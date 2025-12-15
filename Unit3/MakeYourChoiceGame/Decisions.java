c9838586da353c97ce74b1221eb12fcc4b49d1e2
NoahKamma <salesnoah1@gmail.com>
2025-12-15 07:43:49 -1000

    Create Decisions.java

---
+++ Unit3/MakeYourChoiceGame/Decisions.java
@@ -0,0 +1,17 @@
+package MakeYourChoiceGame;
+
+public class Decisions {
+	
+	public String expedition()
+	{
+		return "Yes, I want money!\n"
+				+ "No, this looks sketchy.\n";
+	}
+	
+	public String oneQuestion()
+	{
+		return "What’s in it for me?\n"
+				+ "Is this superhero work?\n"
+				+ "Why is your name TheMan67?\n";
+	}
+}
