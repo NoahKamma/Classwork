5d9d5a077d6b35d91557fc5846ac088b1e9cf9ac
NoahKamma <salesnoah1@gmail.com>
2025-12-15 07:43:33 -1000

    Create Text.java

---
+++ Unit3/MakeYourChoiceGame/Text.java
@@ -0,0 +1,17 @@
+package MakeYourChoiceGame;
+
+public class Text {
+
+	//The amount of lines
+	
+	static TheMan67 theMan67 = new TheMan67();
+	static Body body = new Body();
+	static Decisions decisions = new Decisions();
+	static Response response = new Response();
+	
+	
+	
+	
+	
+	
+}
