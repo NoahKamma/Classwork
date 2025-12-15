ca56a56a0c5f4a0740a4c627b6b0e96f22cd3d23
NoahKamma <salesnoah1@gmail.com>
2025-12-15 07:44:29 -1000

    Create TheMan67.java

---
+++ Unit3/MakeYourChoiceGame/TheMan67.java
@@ -0,0 +1,33 @@
+package MakeYourChoiceGame;
+
+public class TheMan67 {
+
+	public String TheManWelcome()
+	{
+		return "Good evening sir.\n"
+				+ "I heard that you were running low on funds.\n"
+				+ "Wanna get a quick buck?\n"
+				+ "";
+	}
+	
+	public String signThis()
+	{
+		return "Alright, first I need to sign you this totally non-binding waiver.\n"
+				+ "For legal reasons.\n";
+	}
+	
+	public String askOne()
+	{
+		return "Congratulations, you are now a temporary dispatcher for ‘The Company’.\n"
+				+ "Your job is to complete the contracts from our loyal subscribers.\n"
+				+ "You will send out a group of three to take care of whatever needs to be done.\n"
+				+ "This includes acquiring objects of interest for clients,\n"
+				+ "Controlling the city and its civilians,\n"
+				+ "And ‘taking care’ of troublesome individuals.\n"
+				+ "Any questions?\n";
+	}
+	
+	
+	
+	
+}
