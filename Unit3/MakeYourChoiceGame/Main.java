5bc767fe10fb8eb4e319bdbf21226d2bd532358c
NoahKamma <salesnoah1@gmail.com>
2025-12-17 14:02:26 -1000

    Update Main.java

--- Unit3/MakeYourChoiceGame/Main.java
+++ Unit3/MakeYourChoiceGame/Main.java
@@ -1,194 +1,63 @@
 package MakeYourChoiceGame;
 
-import java.awt.Color;
 import java.util.Scanner;
-import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
 
+public class character {
 
-public class Main {
-
-	//Dot operator object console, to "print" and do visual stuff; the extra, special-credit jframe
-	public static Console console = new Console();
 	
-	//Increments for nextStage() for callbacks from console
-	static int stage = 0;
+	//Initialize private variables
+	private double favor;
+	private String name = new String("");
+	private int money=0;
+	private String line;
+	private Scanner input = new Scanner(System.in);
 	
-	/*These variables determine the answer the player has chosen from console
-	 * answerDecisions->Changes depending on decision chosen 1-3
-	 * answerString->Taken from a string the player inputs
-	 */
-	static int answerDecisions = 0;
-	static String answerString = new String ("");
+	//Favor is how much the character likes/favors the player. 0.0 for lowest - 1.0 highest
 	
-	//Static variable for player name is established early game
-	static String name = new String("");
-	static int money = 0;
-	
-	//Initialize static characters
-	static character TheMan67 = new character("TheMan67");
-	static character player = new character("");
-	
-	
-	
-	
-	
-	public static void main(String[] args) 
+	public character(String name)
 	{
-		//Self-explanatory
-		initiateGame();
-
-		
-	}
-	
-	//***************
-	//Order of game events goes here from top to bottom
-	
-	/*
-	 * TheMan67 greets player and prompts a waiver
-	 */
-	public static void initiateGame()
-	{
-		
-		console.setSize(600,900);
-		TheMan67.speak(Text.theMan67.TheManWelcome());
-		
-	}
-
-	/*
-	 * The waiver body is printed and player inputs in textfield
-	 */
-	public static void one()
-	{
-		console.promptDecisions(Text.decisions.expedition(),"Yes","No");
+		this.name=name;
+		setFavor(0.5);
 	}
 	
 	/*
-	 * Set the new player name
+	 * This sets the favor value to the passed double
 	 */
-	public static void two()
+	public void setFavor(double duo)
 	{
-		if(answerDecisions==2)
-		{
-			console.printLine("Ending - Skeptical Civillian",Color.LIGHT_GRAY);
-		}
-		else
-		{
-			TheMan67.speak(Text.theMan67.signThis());
-		}
+		favor=duo;
 	}
 	
 	/*
-	 * TheMan67 instructs player to ask one question
+	 * Changes favor value by the passed double
 	 */
-	public static void three()
+	public void changeFavor(double duo)
 	{
-		console.printBody(Text.body.waiver());
-		console.promptString();
+		favor+=duo;
 	}
 	
-	/*
-	 * Player decides the question
-	 */
-	public static void four()
+	public double getFavor()
 	{
-		console.clear();
-		name=answerString;
-		player.setName(name);
-		
-		TheMan67.speak(Text.theMan67.askOne());
+		return favor;
 	}
 	
-	/*
-	 * TheMan67 responds to player's question
-	 */
-	public static void five()
+	public void setName(String str)
 	{
-		console.promptDecisions(Text.decisions.oneQuestion(),"What","hero","67");
+		name=str;
 	}
 	
-	public static void six()
+	public String getName()
 	{
-		TheMan67.respond(Text.response.oneQuestions());
+		return name;
 	}
 	
-	public static void seven()
+	public void speak(String dialogue)
 	{
-		
+		Console.printDialogue(name,dialogue);
 	}
 	
-	public static void eight()
+	public void respond(String response)
 	{
-		
+		Console.printResponse(name,response);
 	}
-	
-	public static void nine()
-	{
-		
-	}
-	
-	
-	//*********************
-	
-	
-	/*A pseudo flow of control.
-	 * 
-	 */
-	public static void callback()
-	{
-		stage++;
-		
-		switch(stage) 
-		{
-	  /*case 0:
-	   * 	initiateGame();
-	   */ //break();
-		case 1:
-			one();
-			break;
-		case 2:
-			two();
-			break;
-		case 3:
-			three();
-			break;
-		case 4:
-			four();
-			break;
-		case 5:
-			five();
-			break;
-		case 6:
-			six();
-			break;
-		case 7:
-			seven();
-			break;
-			
-		case 8:
-			eight();
-			break;
-		case 9:
-			nine();
-			break;
-		case 10:
-			
-			break;
-		case 11:
-		
-			break;
-		case 12:
-		
-			break;
-		case 13:
-		
-			break;
-		}
-		
-	}
-	
-	//********************
-	
 }
-
-	
