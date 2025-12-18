886e268c31b5d0ea6887394bd365019db7bc5f3f
NoahKamma <salesnoah1@gmail.com>
2025-12-17 14:04:01 -1000

    Create TextRoman.java

---
+++ Unit3/MakeYourChoiceGame/TextRoman.java
@@ -0,0 +1,12 @@
+package MakeYourChoiceGame;
+
+public class TextRoman {
+
+	public static String toJob()
+	{
+		return  "Bro wth\n"
+				+ "i thought this a supervillian group thing\n"
+				+ "i want out NOW!!!!!!!!!!!\n";
+	}
+	
+}
