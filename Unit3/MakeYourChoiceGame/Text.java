229669a86c07d5eb109f168bbc020b21cde462a8
NoahKamma <salesnoah1@gmail.com>
2025-12-17 14:03:28 -1000

    Update Text.java

--- Unit3/MakeYourChoiceGame/Text.java
+++ Unit3/MakeYourChoiceGame/Text.java
@@ -4,7 +4,10 @@ public class Text {
 
 	//The amount of lines
 	
-	static TheMan67 theMan67 = new TheMan67();
+	static TextTheMan67 theMan67 = new TextTheMan67();
+	static TextRoman roman = new TextRoman();
+	
+	
 	static Body body = new Body();
 	static Decisions decisions = new Decisions();
 	static Response response = new Response();
