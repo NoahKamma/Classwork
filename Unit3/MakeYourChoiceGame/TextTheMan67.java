534e7971b8fda6c7d4546f8e1b248d9e23d30ff7
NoahKamma <salesnoah1@gmail.com>
2025-12-17 14:03:09 -1000

    Update and rename TheMan67.java to TextTheMan67.java

--- Unit3/MakeYourChoiceGame/TextTheMan67.java
+++ Unit3/MakeYourChoiceGame/TextTheMan67.java
@@ -1,8 +1,8 @@
 package MakeYourChoiceGame;
 
-public class TheMan67 {
+public class TextTheMan67 {
 
-	public String TheManWelcome()
+	public static String TheManWelcome()
 	{
 		return "Good evening sir.\n"
 				+ "I heard that you were running low on funds.\n"
@@ -10,13 +10,13 @@ public class TheMan67 {
 				+ "";
 	}
 	
-	public String signThis()
+	public static String signThis()
 	{
 		return "Alright, first I need to sign you this totally non-binding waiver.\n"
 				+ "For legal reasons.\n";
 	}
 	
-	public String askOne()
+	public static String askOne()
 	{
 		return "Congratulations, you are now a temporary dispatcher for ‘The Company’.\n"
 				+ "Your job is to complete the contracts from our loyal subscribers.\n"
@@ -27,6 +27,13 @@ public class TheMan67 {
 				+ "Any questions?\n";
 	}
 	
+	public static String toJob()
+	{
+		return "This is one of your three subordinates that you will coordinate.\n"
+				+ "Like you, they are being trained for a full-time job.\n"
+				+ "For now, expect small-scale contracts.\n"
+				+ "I leave it up to you to learn along the way.\n";
+	}
 	
 	
 	
