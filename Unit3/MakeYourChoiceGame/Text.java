caa13c46aa2bdd9f52b3db31373b2d0d19bf4248
NoahKamma <salesnoah1@gmail.com>
2025-12-19 07:51:19 -1000

    Update Text.java

--- Unit3/MakeYourChoiceGame/Text.java
+++ Unit3/MakeYourChoiceGame/Text.java
@@ -2,12 +2,16 @@ package MakeYourChoiceGame;
 
 public class Text {
 
-	//The amount of lines
-	
+	/*
+	*Static objects to refer to character dialogue via dot operators
+	*EX: Text.roman.getJob();
+	*/
 	static TextTheMan67 theMan67 = new TextTheMan67();
 	static TextRoman roman = new TextRoman();
 	
-	
+	/*
+	*Static objects to refer to dialogue, response, and text body strings
+	*/
 	static Body body = new Body();
 	static Decisions decisions = new Decisions();
 	static Response response = new Response();
