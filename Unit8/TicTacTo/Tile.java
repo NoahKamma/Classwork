e8489dc341399c7e87c30ca38f7258b1dc27c5f2
NoahKamma <salesnoah1@gmail.com>
2026-03-06 09:22:18 -1000

    Create Tile.java

---
+++ Unit8/TicTacTo/Tile.java
@@ -0,0 +1,31 @@
+package ticTacTo;
+
+import javax.swing.JButton;
+
+
+public class Tile extends JButton{
+
+	JButton button;
+	
+	
+	
+	public Tile()
+	{
+		//Substantiate button and determine size
+		
+		
+				button=new JButton(" ");
+				button.setSize(200,200);
+				button.setVisible(true);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
