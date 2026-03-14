8a17a65972cbd0bb8c867202e2a5f06f35c7b69b
NoahKamma <salesnoah1@gmail.com>
2026-03-13 23:22:40 -1000

    Create main.java

---
+++ Unit8/connectFour/main.java
@@ -0,0 +1,21 @@
+package connectFour;
+
+import java.awt.Color;
+
+public class Main {
+
+	
+	
+	public static void main(String[] args) {
+		
+		ConnectFourBoard board = new ConnectFourBoard();
+		System.out.println("Play with 2 people in this interactive game!");
+		
+	}
+		
+		
+		
+		
+		
+
+}
